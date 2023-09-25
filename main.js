5771b4f764d148763973ac8fde74c487838815f6
BarbaraBravoRedondo <barbarabravoredondo@gmail.com>
2023-09-25 11:40:43 +0200

    fetch added

--- main.js
+++ main.js
@@ -16,29 +16,21 @@ const labelMessageError = document.querySelector('.js-label-error');
 const input_search_desc = document.querySelector('.js_in_search_desc');
 const input_search_race = document.querySelector('.js_in_search_race');
 
-//Objetos con cada gatito
-const kittenData_1 = {
-  image: 'https://dev.adalab.es/gato-siames.webp',
-  name: 'Anastacio',
-  desc: 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.',
-  race: 'Siamés',
-};
-const kittenData_2 = {
-  image: 'https://dev.adalab.es/sphynx-gato.webp',
-  name: 'Fiona',
-  desc: 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.',
-  race: 'Sphynx',
-};
-const kittenData_3 = {
-  image: 'https://dev.adalab.es/maine-coon-cat.webp',
-  name: 'Sofia',
-  desc: ' Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.',
-  race: '',
-};
+const GITHUB_USER = 'BarbaraBravoRedondo';
+const SERVER_URL = `https://dev.adalab.es/api/kittens/${GITHUB_USER}`;
 
-const kittenDataList = [kittenData_1, kittenData_2, kittenData_3];
+let kittenDataList = [];
 
 //Funciones
+
+fetch(SERVER_URL)
+  .then((response) => response.json())
+  .then((data) => {
+    kittenDataList = data.results;
+    renderKittenList(kittenDataList);
+    console.log(kittenDataList);
+  });
+
 function renderKitten(kittenData) {
   let html = '';
   if (kittenData.race === '') {
@@ -66,6 +58,7 @@ function renderKitten(kittenData) {
 
 function renderKittenList(kittenDataList) {
   listElement.innerHTML = '';
+
   for (const kittenItem of kittenDataList) {
     listElement.innerHTML += renderKitten(kittenItem);
   }
@@ -137,16 +130,7 @@ function filterKitten(event) {
     );
 
   renderKittenList(kittenListFiltered);
-
-  /*   for (const kittenItem of kittenDataList) {
-    if (kittenItem.desc.includes(descrSearchText)) {
-      listElement.innerHTML += renderKitten(kittenItem);
-    }
-  } */
 }
-
-//Mostrar el litado de gatitos en ell HTML
-renderKittenList(kittenDataList);
 
 //Eventos
 linkNewFormElememt.addEventListener('click', handleClickNewCatForm);
